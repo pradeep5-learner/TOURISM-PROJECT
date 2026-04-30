e22d9613ba9ebef3854b6aeb3a5d962703c7d534
pradeep5-learner <reddipradeep1467@gmail.com>
2026-04-30 21:02:02 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const cors = require("cors");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const Place = require('./models/place');
-const User = require('./models/User');
+const User = require('./models/user');
 
 const app = express();
 app.use(cors());
